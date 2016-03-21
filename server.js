eac50f37135870212591922116cfb6a80c10f198
Mhaieiei <mai_junes@hotmail.com>
2016-03-22 02:50:43 +0700

    fixed port 2

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ var app = require('./app')(db);
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-http.createServer(app).listen(server_port, server_host, function(){
-	console.log('Express server listening on port ' + app.get('env'));
-});
+http.createServer(app).listen(process.env.PORT || 5000);
+
