a4a6a202df971238e12b79e3ab7ddac32f541bff
Mhaieiei <mai_junes@hotmail.com>
2016-03-21 22:05:03 +0700

    change

--- model/WorkflowExecution.model.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = new mongoose.Schema({
-	templateId: mongoose.Schema.Types.ObjectId
-});
-
-module.exports = mongoose.model('WorkflowExecution', schema);
