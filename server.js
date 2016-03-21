ed02a1d49920ce237f511b00225a23eeea5074fd
Mhaieiei <mai_junes@hotmail.com>
2016-03-22 02:23:32 +0700

    fixed port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var db =  require('./lib/dmsDb');
 var app = require('./app')(db);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+http.createServer(app).listen(server_port, server_host, function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
